895f2d87737f333e193e8575cceb08d7876e3ace
revanth05-tech <revanthyegireddi@gmail.com>
2025-09-21 21:42:18 +0530

    simple updates

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ if (app.get('env') === 'development') {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Middleware
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
