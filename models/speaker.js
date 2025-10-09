4b15f242c3e4789f7a6cb27bc707721b0a7c209e
revanth05-tech <revanthyegireddi@gmail.com>
2025-10-09 07:24:58 +0530

    new speaker model

--- models/speaker.js
+++ models/speaker.js
@@ -7,7 +7,12 @@ const speakerSchema = new mongoose.Schema({
   title: String,
   expertise: String,
   fee: { type: Number, default: 0 },
-  bio: String
+  bio: String,
+  createdBy: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Speaker', speakerSchema);
